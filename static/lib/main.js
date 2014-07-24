ae47f295ee9f6d98162bae2d5662e1cba9bac87d
psychobunny <psycho.bunny@hotmail.com>
2014-07-24 17:17:53 -0400

    pinning almost works perfectabl

--- static/lib/main.js
+++ static/lib/main.js
@@ -9,45 +9,51 @@ $('document').ready(function() {
 			tid = tid[1];
 
 			$.get(RELATIVE_PATH + '/api/events/tid/' + tid, function(events) {
-				for (var ev in events) {
-					if (events.hasOwnProperty(ev)) {
-						var data = events[ev];
-
-						switch (data.eventType) {
-						case 'pin' :
-							var str = 'events:topic.' + (data.isPinned ? 'pinned' : 'unpinned');
-							data.content = translator.compile(str, RELATIVE_PATH + '/users/' + data.userslug, data.username, utils.toISOString(data.timestamp));
-							data.class = data.isPinned ? 'success' : 'warning';
-							break;
-						default :
-							continue;
-						}
-
-
-						templates.parse('events/topic', data, function(tpl) {
-							translator.translate(tpl, function(content) {
-
-								var rows = $('li.post-row');
-								rows.each(function(idx) {
-									var $this = $(this),
-										nextRow = rows.eq(idx + 1),
-										nextRowTimestamp = nextRow.attr('data-timestamp') ? nextRow.attr('data-timestamp') : data.timestamp + 1;
-
-									console.log($this.attr('data-timestamp'), data.timestamp, nextRowTimestamp);
-									if ($this.attr('data-timestamp') < data.timestamp && nextRowTimestamp > data.timestamp) {
-										$(content).insertAfter($this).find('.timeago').timeago();
-										$this.css('margin-bottom', '0px');
-										return false;
-									}
-								});
-							});
-						});
+				$.each(events, function(idx, data) {
+					switch (data.eventType) {
+					case 'pin' :
+						var str = 'events:topic.' + (data.isPinned ? 'pinned' : 'unpinned');
+						data.content = translator.compile(str, RELATIVE_PATH + '/users/' + data.userslug, data.username, utils.toISOString(data.timestamp));
+						data.class = data.isPinned ? 'success' : 'warning';
+						break;
+					default :
+						return true;
 					}
-				}
+
+
+					createEventRow(data, idx);
+				});
 			});
-
-
-
 		}
 	});
+
+
+	function createEventRow(data, idx) {
+		templates.parse('events/topic', data, function(tpl) {
+			translator.translate(tpl, function(content) {
+
+				var rows = $('li.post-row');
+				rows.each(function(idx) {
+					var $this = $(this),
+						nextRow = rows.eq(idx + 1),
+						nextRowTimestamp = nextRow.attr('data-timestamp') ? nextRow.attr('data-timestamp') : data.timestamp + 1;
+
+					if ($this.attr('data-timestamp') < data.timestamp && nextRowTimestamp > data.timestamp) {
+						var contentEl = $(content).insertAfter($this).find('.timeago').timeago();
+
+						// BEGIN: very lavender specific styling here. Not sure how this pans out on other themes.
+						$this.css('margin-bottom', '0px');
+						if (!contentEl.next().length) {
+							$('.bottom-post-bar').css('margin-top', '0px');
+						} else {
+							contentEl.next().css('margin-top', '0px');	
+						}
+						// END: very lavender specific styling here. Not sure how this pans out on other themes.
+						
+						return false;
+					}
+				});
+			});
+		});
+	}
 });
