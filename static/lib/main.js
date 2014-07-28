21b648c744501ee6bc8abb6300205b3f802cd97f
psychobunny <psycho.bunny@hotmail.com>
2014-07-27 23:15:25 -0400

    clientside logic for moved event

--- static/lib/main.js
+++ static/lib/main.js
@@ -18,6 +18,7 @@ $('document').ready(function() {
 	socket.on('event:topic_unpinned', getEventsData);
 	socket.on('event:topic_locked', getEventsData);
 	socket.on('event:topic_unlocked', getEventsData);
+	socket.on('event:topic_moved', getEventsData);
 
 
 	function getEventsData(data) {
@@ -25,16 +26,28 @@ $('document').ready(function() {
 
 		$.get(RELATIVE_PATH + '/api/events/tid/' + tid, function(events) {
 			$.each(events, function(idx, data) {
+				var timestamp = utils.toISOString(data.timestamp),
+					userUrl = RELATIVE_PATH + '/users/' + data.userslug;
+
 				switch (data.eventType) {
 				case 'pin' :
 					var str = 'events:topic.' + (data.isPinned ? 'pinned' : 'unpinned');
-					data.content = translator.compile(str, RELATIVE_PATH + '/users/' + data.userslug, data.username, utils.toISOString(data.timestamp));
+
+					data.content = translator.compile(str, userUrl, data.username, timestamp);
 					data.class = data.isPinned ? 'success' : 'warning';
 					break;
 				case 'lock' :
 					var str = 'events:topic.' + (data.isLocked ? 'locked' : 'unlocked');
-					data.content = translator.compile(str, RELATIVE_PATH + '/users/' + data.userslug, data.username, utils.toISOString(data.timestamp));
+					
+					data.content = translator.compile(str, userUrl, data.username, timestamp);
 					data.class = data.isLocked ? 'success' : 'warning';
+					break;
+				case 'move' :
+					var from = data.categories.from,
+						to = data.categories.to;
+
+					data.content = translator.compile(str, userUrl, data.username, from.slug, from.name, to.slug, to.name, timestamp);
+					data.class = 'info';
 					break;
 				default :
 					return true;
